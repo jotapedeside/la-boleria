facca09524d34e98b254ac317e54aa84e400f1b3
C <jotapedeside@gmail.com>
2022-10-25 16:26:00 -0300

    chore(deploy): preparing for deploy

--- src/models/index.js
+++ src/models/index.js
@@ -4,6 +4,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 const { Pool } = pg;
 
-const connection = new Pool({connectionString: process.env.DATABASE_URL});
+const connection = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 export default connection;
